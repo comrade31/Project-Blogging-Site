7ed26688c7b0e38bfc33f9e08372423e5d7c8d70
Saquib Akhtar <saquibmohammad38@gmail.com>
2022-11-14 13:26:58 +0530

    added

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express= require("express")
+const bodyParser = require ("body-parser")
+const {default:mongoose} = require ("mongoose")
+// const router = require("./routes/route")
+const app = express()
+
+
+
+app.use(bodyParser.json())
+app.use (bodyParser.urlencoded({extended : true}))
+
+mongoose.connect("mongodb+srv://ashi:jhansi284205@myfirstcluster.tfihevu.mongodb.net/project1",{ useNewUrlParser: true})
+.then( ()=> console.log("mongodb is connected"))
+.catch(err => console.log(err))
+
+//app.use('/',router)
+
+app.listen(process.env.poorts || 3000, function(){
+   console.log('express app running on port'+ (process.env.poorts || 3000))
+})
