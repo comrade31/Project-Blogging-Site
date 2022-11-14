e82510330a96114ccd68e51819f3ffc726eda533
ashishri04 <ashi.shri42@gmail.com>
2022-11-14 14:19:17 +0530

    completed

---
+++ src/models/blogModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+const objectId = mongoose.schema.types.objectId;
+
+
+const blogSchema = new mongoose.Schema( {
+    title: {
+        type:string,
+        require:true
+    },
+    body: { 
+        type:string,
+        require:true
+    },
+    authorId:{
+        type: objectId,
+        ref:"author"
+    },
+    tags:{
+        type:[String]
+    },
+    category:{
+        type: String,
+        require:true
+    },
+    subcategory:[String],
+    deletedAt:Date,
+    isDeleted:{
+        type:Boolean,
+        default: false
+    },
+    publishedAt: Date,
+    isPublished:{
+        type: Boolean,
+        default: false
+    }
+},{timestamps: true});
+
+
+module.exports = mongoose.model("Blog",blogSchema)
